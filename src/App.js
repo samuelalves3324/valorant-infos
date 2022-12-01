c55308441de09115cb3aab004b46d6774da9bf98
Samuel Alves <samucahhh@gmail.com>
2022-12-01 01:21:53 -0300

    add api functions

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Ola</h1>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,26 @@
+export const getAgentsList = async () => {
+  const response = await fetch('https://valorant-api.com/v1/agents?language=pt-BR');
+  const data = await response.json();
+  return data.data;
+}
+
+export const getAgentByName = async (name) => {
+  const agents = await getAgentsList();
+  return agents.find((agent) => agent.displayName = name);
+}
+
+export const getWeaponsList = async () => {
+  const response = await fetch('https://valorant-api.com/v1/weapons?language=pt-BR');
+  const data = await response.json();
+  return data.data;
+}
+
+export const getWeaponByName = async (name) => {
+  const weapons = await getWeaponsList();
+  return weapons.find((weapon) => weapon.displayName === name);
+}
+
+export const getWeaponSkinsByName = async (name) => {
+  const weapon = await getWeaponByName(name);
+  return weapon.skins;
+}
