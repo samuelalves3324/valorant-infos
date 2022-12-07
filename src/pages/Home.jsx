0653e90652991e3bb30b9b1090848f71b5edb742
Samuel Alves <samucahhh@gmail.com>
2022-12-07 08:04:55 -0300

    add home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
 import Header from '../components/Header';
+import Button from '../components/Button';
+import Footer from '../components/Footer';
+import { Link } from 'react-router-dom';
+import '../styles/Home.css';
 
 class Home extends React.Component {
   render() {
     return (
-      <Header />
+      <div>
+        <Header />
+        <main>
+            <div className='home-page'>
+              <div className='home-container'>
+                <h1>vALORANT INFOS</h1>
+                <p>Aqui você encontra informações atualizadas sobre valorant!</p>
+                <div>
+                  <Link to="/agentes">
+                    <Button type='button' text='AGENTES' className='home-page-button' />
+                  </Link>
+                  <Button type='button' text='ARMAS' className='home-page-button' />
+                </div>
+              </div>
+            </div>
+          </main>
+        <Footer />
+      </div>
     );
   }
 }
