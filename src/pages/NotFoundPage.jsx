fd9ad3fcb6c6bbc1e29d33ee9d3e715533e123db
Samuel Alves <samucahhh@gmail.com>
2022-12-12 16:29:04 -0300

    delete readme text

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Header from '../components/Header';
-import Footer from '../components/Footer';
 import '../styles/NotFoundPage.css';
 
 class NotFoundPage extends React.Component {
